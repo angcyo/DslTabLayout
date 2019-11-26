a57f23ab2bf13a7fc64892c8507c5061a3b274a4
angcyo <angcyo@126.com>
2019-11-26 16:55:23 +0800

    + demo

--- Sample/src/main/java/com/angcyo/dsltablayout/demo/SlidingFragment.kt
+++ Sample/src/main/java/com/angcyo/dsltablayout/demo/SlidingFragment.kt
@@ -3,6 +3,7 @@ package com.angcyo.dsltablayout.demo
 import android.os.Bundle
 import androidx.core.graphics.toColorInt
 import com.angcyo.dsladapter.DslViewHolder
+import com.angcyo.dsladapter.dpi
 import com.angcyo.tablayout.DslTabLayout
 import com.angcyo.tablayout.TabIndicator
 
@@ -24,6 +25,33 @@ class SlidingFragment : BaseTabLayoutFragment() {
                     itemHolder.v<DslTabLayout>(R.id.tab_layout).apply {
                         setBackgroundColor("#666466".toColorInt())
                         addTabLayout(this)
+                        tabIndicator.indicatorWidth = 4 * dpi
+                        tabIndicator.indicatorDrawable =
+                            getDrawable(R.drawable.indicator_white_line)
+                        tabIndicator.indicatorStyle = TabIndicator.INDICATOR_STYLE_TOP
+                    }
+                }
+            }
+
+            DslTabLayoutItem()() {
+                onItemBindOverride = { itemHolder, _, _ ->
+                    itemHolder.v<DslTabLayout>(R.id.tab_layout).apply {
+                        setBackgroundColor("#009688".toColorInt())
+                        addTabLayout(this)
+                        tabIndicator.indicatorWidth = -2
+                        tabIndicator.indicatorDrawable =
+                            getDrawable(R.drawable.indicator_white_line)
+                        tabIndicator.indicatorStyle = TabIndicator.INDICATOR_STYLE_TOP
+                    }
+                }
+            }
+
+            DslTabLayoutItem()() {
+                onItemBindOverride = { itemHolder, _, _ ->
+                    itemHolder.v<DslTabLayout>(R.id.tab_layout).apply {
+                        setBackgroundColor("#379DE4".toColorInt())
+                        addTabLayout(this)
+                        tabIndicator.indicatorWidth = -1
                         tabIndicator.indicatorDrawable =
                             getDrawable(R.drawable.indicator_white_line)
                         tabIndicator.indicatorStyle = TabIndicator.INDICATOR_STYLE_TOP
@@ -35,15 +63,74 @@ class SlidingFragment : BaseTabLayoutFragment() {
             DslTabLayoutItem()() {
                 onItemBindOverride = { itemHolder, _, _ ->
                     itemHolder.v<DslTabLayout>(R.id.tab_layout).apply {
-                        setBackgroundColor("#7F3737".toColorInt())
+                        setBackgroundColor("#EE6F5E".toColorInt())
                         addTabLayout(this)
+                        tabIndicator.indicatorWidth = 10 * dpi
+                        tabIndicator.indicatorHeight = 10 * dpi
                         tabIndicator.indicatorDrawable =
-                            getDrawable(R.drawable.indicator_white_line)
+                            getDrawable(R.drawable.ic_triangle)
+                        tabIndicator.indicatorStyle = TabIndicator.INDICATOR_STYLE_BOTTOM
                     }
                 }
             }
 
+            DslTabLayoutItem()() {
+                onItemBindOverride = { itemHolder, _, _ ->
+                    itemHolder.v<DslTabLayout>(R.id.tab_layout).apply {
+                        setBackgroundColor("#7F3737".toColorInt())
+                        addTabLayout(this)
+                        tabIndicator.indicatorWidth = -2
+                        tabIndicator.indicatorDrawable =
+                            getDrawable(R.drawable.indicator_white_line)
+                        tabIndicator.indicatorStyle = TabIndicator.INDICATOR_STYLE_BOTTOM
+                    }
+                }
+            }
 
+            DslTabLayoutItem()() {
+                onItemBindOverride = { itemHolder, _, _ ->
+                    itemHolder.v<DslTabLayout>(R.id.tab_layout).apply {
+                        setBackgroundColor("#A632A5".toColorInt())
+                        addTabLayout(this)
+                        tabIndicator.indicatorWidth = -1
+                        tabIndicator.indicatorDrawable =
+                            getDrawable(R.drawable.indicator_white_line)
+                        tabIndicator.indicatorStyle = TabIndicator.INDICATOR_STYLE_BOTTOM
+                    }
+                }
+            }
+
+            //指示器在背部
+            DslTabLayoutItem()() {
+                onItemBindOverride = { itemHolder, _, _ ->
+                    itemHolder.v<DslTabLayout>(R.id.tab_layout).apply {
+                        setBackgroundColor("#6B8DB3".toColorInt())
+                        addTabLayout(this)
+                        tabIndicator.indicatorDrawable =
+                            getDrawable(R.drawable.indicator_round_background)
+                        tabIndicator.indicatorHeight = -2
+                        tabIndicator.indicatorWidth = -2
+                        tabIndicator.indicatorWidthOffset = 20 * dpi
+                        tabIndicator.indicatorHeightOffset = -15 * dpi
+                        tabIndicator.indicatorStyle = TabIndicator.INDICATOR_STYLE_BACKGROUND
+                    }
+                }
+            }
+            DslTabLayoutItem()() {
+                onItemBindOverride = { itemHolder, _, _ ->
+                    itemHolder.v<DslTabLayout>(R.id.tab_layout).apply {
+                        setBackgroundColor("#F7CE4A".toColorInt())
+                        addTabLayout(this)
+                        tabIndicator.indicatorDrawable =
+                            getDrawable(R.drawable.ic_love)
+                        tabIndicator.indicatorHeight = -1
+                        tabIndicator.indicatorWidth = 55 * dpi
+//                        tabIndicator.indicatorWidthOffset = 20 * dpi
+//                        tabIndicator.indicatorHeightOffset = -15 * dpi
+                        tabIndicator.indicatorStyle = TabIndicator.INDICATOR_STYLE_BACKGROUND
+                    }
+                }
+            }
 
             DslViewPagerItem(childFragmentManager)() {
                 onItemBindOverride = { itemHolder, _, _ ->
