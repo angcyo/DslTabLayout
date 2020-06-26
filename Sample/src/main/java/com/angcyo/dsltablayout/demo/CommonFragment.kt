1a920d11ce4abada1108541173d285d709ffd955
angcyo <angcyo@126.com>
2020-06-26 11:45:34 +0800

    + drawable selected demo

--- Sample/src/main/java/com/angcyo/dsltablayout/demo/CommonFragment.kt
+++ Sample/src/main/java/com/angcyo/dsltablayout/demo/CommonFragment.kt
@@ -57,6 +57,13 @@ class CommonFragment : BaseTabLayoutFragment() {
                         configTabLayoutConfig {
                             tabEnableGradientColor = true
                             tabSelectColor = tabIndicator.indicatorColor
+
+                            onGetTextStyleView = { itemView, index ->
+                                itemView.findViewById(R.id.text_view)
+                            }
+                            onGetIcoStyleView = { itemView, index ->
+                                itemView.findViewById(R.id.text_view)
+                            }
                         }
                     }
                 }
