c2d6f3d87722c92f951fe9382f0412fb9045a072
angcyo <angcyo@126.com>
2020-07-14 17:03:28 +0800

    1.5.6

--- TabLayout/src/main/java/com/angcyo/tablayout/DslSelector.kt
+++ TabLayout/src/main/java/com/angcyo/tablayout/DslSelector.kt
@@ -128,8 +128,8 @@ open class DslSelector {
             }
         }
         if (dslSelectIndex in visibleViewList.indices) {
-            if (!visibleViewList[dslSelectIndex].isSelected) {
-                visibleViewList[dslSelectIndex].isSelected = true
+            if (!visibleViewList[dslSelectIndex].isSe()) {
+                visibleViewList[dslSelectIndex].setSe(true)
             }
         } else {
             //如果当前选中的索引, 不在可见视图列表中
